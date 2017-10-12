6df133d4a566240f935f3adb62583a32750b7984
Phelipe Rocha <phelipe.rocha@celsolisboa.edu.br>
2017-10-12 20:38:47 -0300

    Config file

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
 module.exports = {
-    // App Settings
-    MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/cuppaOAuth',
-    // MONGO_URI: process.env.MONGO_URI || 'mongodb://username:password@ds111489.mlab.com:11489/cuppaoauth',
-    TOKEN_SECRET: process.env.TOKEN_SECRET || 'YOUR_UNIQUE_JWT_TOKEN_SECRET',
-
-    // OAuth 2.0
-    FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || 'xxxxxxx',
-    GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'xxxxxxx',
-    LINKEDIN_SECRET: process.env.LINKEDIN_SECRET || '95eJcM08pCn9Leyy',
+    TOKEN_SECRET: 'o3s9FhA^lZB85kVC3pAm!a1AinLOUrUFsrbNldeF3rUadP$ZF9sZOq!C6H&cCGKc5DFYN2@4@N*wLV4CtkxLjqq&sA#nrsuJfo3la!$#dfsqblKuWAlkA9u#YQGcN$FKPsup6ypgHJdyGS2hDtF2wbJm!3KePJ4x6Q241eJofY42$eBDY8hrSw&Sw2cs0yaS6A@e^Jc8cRWeNN%9!v2aR!FgU2$2s8UxFtawfgk%6O45dcCgo!wTcQmiO^qN8l50',
+    LINKEDIN_SECRET: '95eJcM08pCn9Leyy',
 };
