af133463d1c69f02b19a54c88fc4ece9752bfb77
Phelipe Rocha <phelipe.rocha@celsolisboa.edu.br>
2017-10-13 18:55:39 -0300

    Get all templates availables

--- controllers/template.controller.js
+++ controllers/template.controller.js
@@ -1,27 +1,7 @@
-exports.getAllTemplates = function (req, res) {
-    var templates = [
-        {
-            id: 1,
-            name: 'Template 1',
-            image: 'http://placehold.it/200x200'
-        },{
-            id: 2,
-            name: 'Template 2',
-            image: 'http://placehold.it/200x200'
-        },{
-            id: 3,
-            name: 'Template 3',
-            image: 'http://placehold.it/200x200'
-        },{
-            id: 4,
-            name: 'Template 4',
-            image: 'http://placehold.it/200x200'
-        },{
-            id: 6,
-            name: 'Template 6',
-            image: 'http://placehold.it/200x200'
-        }
-    ];
+let Template = require('../models/template');
 
-    return res.status(200).send(templates);
+exports.getAllTemplates = function (req, res) {
+    Template.getAllTemplates(result => {
+        return res.status(200).send(result);
+    });
 };
