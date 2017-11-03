675e153f0baae985cc77648b84122090d750a466
Phelipe Rocha <phelipe.rocha@celsolisboa.edu.br>
2017-11-03 10:05:13 -0200

    Enable cors to all host

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var server = app.listen(app.get('port'), app.get('host'), function() {
+var server = app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
 });
 
