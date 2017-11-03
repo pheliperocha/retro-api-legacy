36f90f1d7f67a0eacac6fa57d5060b63c8b94a92
Phelipe Rocha <phelipe.rocha@celsolisboa.edu.br>
2017-11-03 12:05:37 -0200

    Retrospectives get template images

--- models/user.js
+++ models/user.js
@@ -1,7 +1,10 @@
 var db = require('../db');
 
 exports.myRetrospectives = function (userId, cb) {
-    let query = "SELECT cd_reuniao as id, nome as title, contexto as context, image, dh_timestamp as date FROM reuniao WHERE cd_usuario = ?";
+    let query = "SELECT cd_reuniao as id, reuniao.nome as title, contexto as context, template.image, reuniao.dh_timestamp as date " +
+        "FROM reuniao " +
+        "JOIN template ON reuniao.cd_template = template.cd_template " +
+        "WHERE reuniao.cd_usuario = ? ";
 
     db.query(query, [userId], function (err, results) {
         if (err) {
